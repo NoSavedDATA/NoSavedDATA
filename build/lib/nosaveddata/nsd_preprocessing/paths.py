19c12bcd220306147abb441bd33fb1c09b535947
nosaveddata <nosaveddata@Z1>
2025-02-14 10:46:47 -0300

    LLaMa XL

--- build/lib/nosaveddata/nsd_preprocessing/paths.py
+++ build/lib/nosaveddata/nsd_preprocessing/paths.py
@@ -1,7 +1,14 @@
 import os
+import yaml
 
 def fname_of(path):
     return path.split(os.sep)[-1].split('.')[0]
+
+def fexists(path):
+    return os.path.exists(path)
+
+def path_with_idx(path,idx):
+    return path.split('.')[0] + '_' + str(idx) + '.' + path.split('.')[1]
 
 def folder_name_of(path):
     return path.split(os.sep)[-2]
@@ -14,3 +21,8 @@ def full_path_of(path):
 
 def mkdir(path):
     os.makedirs(path,exist_ok=True)
+
+def read_yaml(path):
+    with open(path, "r") as file:
+        yaml_dict = yaml.safe_load(file)
+    return yaml_dict
