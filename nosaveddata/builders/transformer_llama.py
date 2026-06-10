9a5f4b314bc294001b64ead201bac5f081daa4b4
NoSavedDATA <nosaveddataoz1@gmail.com>
2026-06-10 13:53:20 -0300

    Update transformer_llama.py

--- nosaveddata/builders/transformer_llama.py
+++ nosaveddata/builders/transformer_llama.py
@@ -30,70 +30,19 @@ class ModelArgs:
 
 class RMSNorm(nn.Module):
     def __init__(self, dim, eps = 1e-6):
-        """
-        Initialize the RMSNorm normalization layer.
-
-        Args:
-            dim (int): The dimension of the input tensor.
-            eps (float, optional): A small value added to the denominator for numerical stability. Default is 1e-6.
-
-        Attributes:
-            eps (float): A small value added to the denominator for numerical stability.
-            weight (nn.Parameter): Learnable scaling parameter.
-
-        """
         super().__init__()
         self.eps = eps
         self.weight = nn.Parameter(torch.ones(dim))
 
     def _norm(self, x):
-        """
-        Apply the RMSNorm normalization to the input tensor.
-
-        Args:
-            x (torch.Tensor): The input tensor.
-
-        Returns:
-            torch.Tensor: The normalized tensor.
-
-        """
         return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
 
     def forward(self, x):
-        """
-        Forward pass through the RMSNorm layer.
-
-        Args:
-            x (torch.Tensor): The input tensor.
-
-        Returns:
-            torch.Tensor: The output tensor after applying RMSNorm.
-
-        """
         output = self._norm(x.float()).type_as(x)
         return output * self.weight
 
 
 def precompute_freqs_cis(dim, end, theta = 10000.0):
-    """
-    Precompute the frequency tensor for complex exponentials (cis) with given dimensions.
-
-    This function calculates a frequency tensor with complex exponentials using the given dimension 'dim'
-    and the end index 'end'. The 'theta' parameter scales the frequencies.
-    The returned tensor contains complex values in complex64 data type.
-
-    Args:
-        dim (int): Dimension of the frequency tensor.
-        end (int): End index for precomputing frequencies.
-        theta (float, optional): Scaling factor for frequency computation. Defaults to 10000.0.
-
-    Returns:
-        torch.Tensor: Precomputed frequency tensor with complex exponentials.
-
-    
-        
-
-    """
     freqs = 1.0 / (theta ** (torch.arange(0, dim, 2)[: (dim // 2)].float() / dim))
     t = torch.arange(end, device=freqs.device)  # type: ignore
     freqs = torch.outer(t, freqs).float()  # type: ignore
@@ -102,55 +51,19 @@ def precompute_freqs_cis(dim, end, theta = 10000.0):
 
 
 def reshape_for_broadcast(freqs_cis, x):
-    """
-    Reshape frequency tensor for broadcasting it with another tensor.
-
-    This function reshapes the frequency tensor to have the same shape as the target tensor 'x'
-    for the purpose of broadcasting the frequency tensor during element-wise operations.
-
-    Args:
-        freqs_cis (torch.Tensor): Frequency tensor to be reshaped.
-        x (torch.Tensor): Target tensor for broadcasting compatibility.
-
-    Returns:
-        torch.Tensor: Reshaped frequency tensor.
-
-    Raises:
-        AssertionError: If the frequency tensor doesn't match the expected shape.
-        AssertionError: If the target tensor 'x' doesn't have the expected number of dimensions.
-    """
     ndim = x.ndim
     assert 0 <= 1 < ndim
     assert freqs_cis.shape == (x.shape[1], x.shape[-1])
     shape = [d if i == 1 or i == ndim - 1 else 1 for i, d in enumerate(x.shape)]
     return freqs_cis.view(*shape)
 
-
+"""
 def apply_rotary_emb(
     xq,
     xk,
     freqs_cis_q,
     freqs_cis_k,
 ):
-    """
-    Apply rotary embeddings to input tensors using the given frequency tensor.
-
-    This function applies rotary embeddings to the given query 'xq' and key 'xk' tensors using the provided
-    frequency tensor 'freqs_cis'. The input tensors are reshaped as complex numbers, and the frequency tensor
-    is reshaped for broadcasting compatibility. The resulting tensors contain rotary embeddings and are
-    returned as real tensors.
-
-    Args:
-        xq (torch.Tensor): Query tensor to apply rotary embeddings.
-        xk (torch.Tensor): Key tensor to apply rotary embeddings.
-        freqs_cis (torch.Tensor): Precomputed frequency tensor for complex exponentials.
-
-    Returns:
-        Tuple[torch.Tensor, torch.Tensor]: Tuple of modified query tensor and key tensor with rotary embeddings.
-
-        
-
-    """
     xq_ = torch.view_as_complex(xq.float().reshape(*xq.shape[:-1], -1, 2))
     xk_ = torch.view_as_complex(xk.float().reshape(*xk.shape[:-1], -1, 2))
     freqs_cis_q = reshape_for_broadcast(freqs_cis_q, xq_)
@@ -158,10 +71,16 @@ def apply_rotary_emb(
     xq_out = torch.view_as_real(xq_ * freqs_cis_q).flatten(3)
     xk_out = torch.view_as_real(xk_ * freqs_cis_k).flatten(3)
     return xq_out.type_as(xq), xk_out.type_as(xk)
-
+"""
+def apply_rotary(x, freqs_cis):
+    x_ = torch.view_as_complex(
+        x.float().reshape(*x.shape[:-1], -1, 2)
+    )
+    freqs_cis = reshape_for_broadcast(freqs_cis, x_)
+    out = torch.view_as_real(x_ * freqs_cis).flatten(-2)
+    return out.type_as(x)
 
 def repeat_kv(x, n_rep):
-    """torch.repeat_interleave(x, dim=2, repeats=n_rep)"""
     bs, slen, n_kv_heads, head_dim = x.shape
     if n_rep == 1:
         return x
@@ -202,7 +121,9 @@ class Attention_Rotary_Embedding(nn.Module):
         k = k.view(B, -1, self.n_head, C // self.n_head) # (B, nh, hs, T)
         v = v.view(B, -1, self.n_head, C // self.n_head) # (B, nh, hs, T)
         
-        q, k = apply_rotary_emb(q, k, freqs_cis, freqs_cis)
+        #q, k = apply_rotary_emb(q, k, freqs_cis, freqs_cis)
+        q = apply_rotary(q, freqs_cis[:q.shape[1]])
+        k = apply_rotary(k, freqs_cis[:k.shape[1]])
         q = q.transpose(1, 2)
         k = k.transpose(1, 2)
         v = v.transpose(1, 2)
@@ -230,12 +151,6 @@ class FFN_LLaMa(nn.Module):
         hidden_dim,
         multiple_of=256, # make SwiGLU hidden layer size multiple of large power of 2
     ):
-        """
-        Args:
-            dim (int): Input dimension.
-            hidden_dim (int): Hidden dimension of the feedforward layer.
-            multiple_of (int): Value to ensure hidden dimension is a multiple of this value.
-        """
         super().__init__()
         hidden_dim = int(2 * hidden_dim / 3)
         # custom dim factor multiplier
@@ -257,24 +172,6 @@ class FFN_LLaMa(nn.Module):
 
 class LLaMa_Block(nn.Module):
     def __init__(self, layer_id, d_model, ffn, nhead, bias=False, dropout=0.1, eps=1e-6, cross_attention=False):
-        """
-        Initialize a TransformerBlock.
-
-        Args:
-            layer_id (int): Identifier for the layer.
-            args (ModelArgs): Model configuration parameters.
-
-        Attributes:
-            n_heads (int): Number of attention heads.
-            dim (int): Dimension size of the model.
-            head_dim (int): Dimension size of each attention head.
-            attention (Attention): Attention module.
-            feed_forward (FeedForward): FeedForward module.
-            layer_id (int): Identifier for the layer.
-            attention_norm (RMSNorm): Layer normalization for attention output.
-            ffn_norm (RMSNorm): Layer normalization for feedforward output.
-
-        """
         super().__init__()
         head_dim = d_model // nhead
         self.attention = Attention_Rotary_Embedding(d_model, nhead, bias=bias, dropout=dropout)
